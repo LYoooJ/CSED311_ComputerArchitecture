679fc3c334562bfbe1ee59aba27d46b854a9a2f6
LYoooJ <limyoojin@postech.ac.kr>
2024-03-02 13:34:04 +0900

    fix some warnings

--- vending_machine/check_time_and_coin.v
+++ vending_machine/check_time_and_coin.v
@@ -20,7 +20,11 @@ module check_time_and_coin(i_input_coin,i_select_item,clk,reset_n,i_trigger_retu
 		// TODO: initiate values
 		wait_time <=0;
 		o_return_coin <=0;
-		temp, x,y,z <=0;
+		temp <= 0;
+		x <= 0;
+		y <= 0;
+		z <= 0;
+		//temp, x,y,z <=0;
 	end
 
 
--- vending_machine/vending_machine.v
+++ vending_machine/vending_machine.v
@@ -63,7 +63,7 @@ module vending_machine (
 		
   	check_time_and_coin check_time_and_coin_module(.i_input_coin(i_input_coin),
   									.i_select_item(i_select_item),
-									.i_trigger_return(i_trigger_return)
+									.i_trigger_return(i_trigger_return),
 									.clk(clk),
 									.reset_n(reset_n),
 									.wait_time(wait_time),
