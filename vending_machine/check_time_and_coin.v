c658ff68167b863b34130f5c5e83bf14e0b0d01b
unknown <heekyung0000@gmail.com>
2024-03-04 16:45:32 +0900

    changed check_time module

--- vending_machine/check_time_and_coin.v
+++ vending_machine/check_time_and_coin.v
@@ -2,58 +2,52 @@
 
 	
 
-module check_time_and_coin(i_input_coin,i_select_item,clk,reset_n,i_trigger_return,wait_time,current_total,o_return_coin);
+module check_time_and_coin(i_input_coin,o_output_item,i_select_item,i_trigger_return,clk,reset_n, wait_time,current_total,coin_value,o_return_coin);
 	input clk;
 	input reset_n;
 	input i_trigger_return; //input 추가
 	input [`kNumCoins-1:0] i_input_coin;
 	input [`kNumItems-1:0]	i_select_item;
-	input [`kNumCoins-1:0] current_total; // o_return coin에서 반환해야할 때 자판기에 남은 돈을 전부 리턴해야하므로 current total input에 추가
+	input [`kNumItems-1:0] o_output_item;
+	input [`kTotalBits-1:0] current_total; // o_return coin에서 반환해야할 때 자판기에 남은 돈을 전부 리턴해야하므로 current total input에 추가
+	input [31:0] coin_value[`kNumCoins-1:0];
 	output reg  [`kNumCoins-1:0] o_return_coin;
 	output reg [31:0] wait_time;
-
-	integer x, y, z;
-	integer temp;
 
 	// initiate values
 	initial begin
 		// TODO: initiate values
 		o_return_coin = 0;
-		wait_time =0;
-		temp =0;
-		x =0;
-		y =0;
-		z =0;
+		wait_time = `kWaitTime;
 	end
 
 
 	// update coin return time
-	always @(i_input_coin, i_select_item) begin
+	always @(i_input_coin,o_output_item, i_trigger_return) begin
 		// TODO: update coin return time
 		// if i_input coin exist or i_select_item is 1, update the return time
-		if( (i_input_coin !=0) || (i_select_item ==1)) wait_time <= 10;
+		if(i_input_coin || o_output_item)begin
+			wait_time <= `kWaitTime;
+		end
+		if(i_trigger_return) begin
+			wait_time <=0;
+		end
+
 	end
 
 	always @(*) begin
 		// TODO: o_return_coin
-		//각각 필요한 동전의 개수
-		x = current_total/1000;
-		y = (current_total%1000)/500;
-		z = ((current_total%1000)%500)/100;
 
 		if(wait_time == 0 || i_trigger_return) begin //o_return_coin을 업데이트 해준다.
 
-			if(x > 0) begin
-				temp = 3'b100;
-				o_return_coin <= o_return_coin + temp;
+			if(current_total >= coin_value[2]) begin
+				o_return_coin = 3'b100;
 			end
-			else if(y>0) begin
-				temp = 3'b010;
-				o_return_coin <= o_return_coin + temp;
+			if(current_total - coin_value[2] >= coin_value[1]) begin
+				o_return_coin = o_return_coin + 3'b010;
 			end
-			else if(z>0) begin
-				temp = 3'b001;
-				o_return_coin <= o_return_coin + temp;
+			if(current_total - coin_value[2]- coin_value[1] >= coin_value[0]) begin
+				o_return_coin = o_return_coin+ 3'b001;
 			end
 
 		end
@@ -62,12 +56,16 @@ module check_time_and_coin(i_input_coin,i_select_item,clk,reset_n,i_trigger_retu
 	always @(posedge clk ) begin
 		if (!reset_n) begin
 		// TODO: reset all states.
-		wait_time <=0;
-		o_return_coin <=0;
+		wait_time <=`kWaitTime;
+		o_return_coin <= 0;
+		end
+
+		if(wait_time == 0) begin
+			wait_time <=0;
 		end
 		else begin
-		// TODO: update all states.
-		if(wait_time > 0) wait_time <= wait_time-1; // decrease waiting time (10,9,8, 로 내려감)
+			wait_time <= wait_time -1;
 		end
+
 	end
 endmodule 
