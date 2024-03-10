25ea1cc63b577b933f5ac107aa248df7ca99fe99
LYoooJ <limyoojin@postech.ac.kr>
2024-03-10 12:48:24 +0900

    modify logic for o_output_item

--- vending_machine/calculate_current_state.v
+++ vending_machine/calculate_current_state.v
@@ -19,6 +19,7 @@ current_total_nxt,wait_time,o_return_coin,o_available_item,o_output_item);
 
 
 	reg [`kTotalBits-1:0] input_total, output_total, return_total;
+	reg [`kNumItems-1:0] output_item;
 	
 	// Combinational logic for the next states
 	always @(*) begin
@@ -26,22 +27,24 @@ current_total_nxt,wait_time,o_return_coin,o_available_item,o_output_item);
 		// You don't have to worry about concurrent activations in each input vector (or array).
 		// Calculate the next current_total state.
 		
-		// Calculate total return coin value
+		// Initialize values
 		input_total = 0;
 		output_total = 0;
 		return_total = 0;
+		output_item = 0;
 
 		// Calculate total input coin value
 		for (i = 0; i < `kNumCoins; i++) begin
 			if (i_input_coin[i]) begin
-				input_total = input_total + coin_value[i]; //bit 수 안 맞음
+				input_total = input_total + coin_value[i];
 			end
 		end
 
 		// Calculate total output coin value (for purchasing items)
 		for (i = 0; i < `kNumItems; i++) begin 
-			if (i_select_item[i] && ((current_total - output_total) >= item_price[i])) begin
+			if (i_select_item[i] && ((current_total - output_total) >= item_price[i])) begin // if the selected item can be dispensed
 				output_total = output_total + item_price[i];
+				output_item[i] = 1;
 			end
 		end	
 
@@ -55,32 +58,29 @@ current_total_nxt,wait_time,o_return_coin,o_available_item,o_output_item);
 		// Calculate next current_total state
 		current_total_nxt = current_total + input_total - output_total - return_total;
 	end
-
-	
 	
 	// Combinational logic for the outputs
 	always @(*) begin
 		// TODO: o_available_item
 		// TODO: o_output_item
-		o_available_item = 4'b0000;
-		o_output_item = 4'b0000;
 
-		if (wait_time > 0) begin
+		// Initialize values
+		o_available_item = 0;
+		o_output_item = 0;
+
+		if (wait_time > 0) begin // if it's within the waiting time
 			for (i = 0; i < `kNumItems; i++) begin
-				if (current_total >= item_price[i]) begin 
+				if (current_total >= item_price[i]) begin // if the item is available
 					o_available_item[i] = 1;
 				end
 			end
 
 			for (i = 0; i < `kNumItems; i++) begin
-				if (i_select_item[i] && (current_total >= item_price[i])) begin //수정해야 함(current_total - 이미 나온 아이템 값 >= item_price[i]여야 함)
+				if (i_select_item[i] && output_item[i]) begin // if the item can be dispensed
 					o_output_item[i] = 1;
 				end
 			end
 		end 
 	end
- 
-	
-
 
 endmodule 
