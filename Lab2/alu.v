216287b7b9aa19515a685760337f1605dfbaa447
LYoooJ <limyoojin@postech.ac.kr>
2024-03-12 20:25:05 +0900

    write module interface

--- Lab2/alu.v
+++ Lab2/alu.v
@@ -1,7 +1,7 @@
-module alu (input alu_op, 
-            input alu_in_1,
-            input alu_in_2,
-            output alu_result,
+module alu (input [3:0] alu_op, 
+            input [31:0] alu_in_1,
+            input [31:0] alu_in_2,
+            output [31:0] alu_result,
             output alu_bcond);
 
 endmodule
---
+++ Lab2/immediate_generator.v
@@ -0,0 +1,4 @@
+module immediate_generator (input [31:0] part_of_inst, 
+                            output [31:0] imm_gen_out);
+
+endmodule
