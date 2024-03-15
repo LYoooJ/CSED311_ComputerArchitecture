e7ed5eec233a3b30867856786cacb40532bf16a2
LYoooJ <limyoojin@postech.ac.kr>
2024-03-16 00:48:09 +0900

    remove bit error

--- Lab2/immediate_generator.v
+++ Lab2/immediate_generator.v
@@ -1,6 +1,6 @@
 module immediate_generator (input [31:0] part_of_inst, 
-                            output [31:0] imm_gen_out);
+                            output reg [31:0] imm_gen_out);
 
-    assign imm_gen_out = {part_of_inst[11], part_of_inst[10:5], part_of_inst[4:1], 1'b0};
+    assign imm_gen_out = {{21{part_of_inst[11]}}, part_of_inst[10:5], part_of_inst[4:1], 1'b0};
 
 endmodule
