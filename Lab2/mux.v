0028fec3f970f1315428f0b5f26ddb808cfbd228
LYoooJ <limyoojin@postech.ac.kr>
2024-03-16 11:17:04 +0900

    modify syntax

--- Lab2/mux.v
+++ Lab2/mux.v
@@ -6,7 +6,11 @@ module mux (input_1, input_2, control, mux_out);
     output reg [31:0] mux_out;
 
     always @(*) begin
-        if (control == 1'b0) mux_out = input_1;
-        else mux_out = input_2;
-end
+        if (control == 1'b0) begin 
+            mux_out = input_1;
+        end
+        else begin 
+            mux_out = input_2;
+        end
+    end
 endmodule
