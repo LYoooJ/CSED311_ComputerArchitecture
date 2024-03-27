de2bc1bc0f8a928591c6d39fa20a7fa302a82315
LYoooJ <limyoojin@postech.ac.kr>
2024-03-28 01:03:22 +0900

    implement control unit

--- Lab3/ControlUnit.v
+++ Lab3/ControlUnit.v
@@ -1,4 +1,6 @@
-module ControlUnit (input part_of_inst,
+module ControlUnit (input [6:0] part_of_inst,
+                    input bcond,
+                    input clk,
                     output reg PCWriteNotCond,
                     output reg PCWrite,
                     output reg IorD,
@@ -11,5 +13,37 @@ module ControlUnit (input part_of_inst,
                     output reg [1:0] ALUSrcB,
                     output reg ALUSrcA,
                     output reg RegWrite);
-  
+
+reg [3:0] current_state;
+wire [3:0] next_state;
+
+micro_controller micro_controller(
+    .current_state(current_state),
+    .opcode(part_of_inst),
+    .branch_taken(bcond),
+    .PCWriteNotCond(PCWriteNotCond),
+    .PCWrite(PCWrite),
+    .IorD(IorD),
+    .MemRead(MemRead),
+    .MemWrite(MemWrite),
+    .MemtoReg(MemtoReg),
+    .IRWrite(IRWrite),
+    .PCSource(PCSource),
+    .ALUOp(ALUOp),
+    .ALUSrcB(ALUSrcB),
+    .ALUSrcA(ALUSrcA),
+    .RegWrite(RegWrite)
+);  
+
+calculate_next_state calculate_next_state(
+    .opcode(part_of_inst),
+    .clk(clk),
+    .current_state(current_state),
+    .next_state(next_state)
+);
+
+always @(next_state) begin
+    current_state <= next_state;
+end
+
 endmodule
