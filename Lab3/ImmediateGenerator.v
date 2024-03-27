382cbd1cf7c313d8435bd003c8ecfb501b80d62f
unknown <heekyung0000@gmail.com>
2024-03-27 16:51:09 +0900

    imm gen module

--- Lab3/ImmediateGenerator.v
+++ Lab3/ImmediateGenerator.v
@@ -1,4 +1,36 @@
-module ImmediateGenerator (input part_of_inst,
-                           output reg imm_gen_out);  
+`include "opcodes.v"
 
-endmodule
+//Imm gen 은 동일하게 가져옴.
+module ImmediateGenerator (input [31:0] part_of_inst,
+                           output reg [31:0] imm_gen_out); 
+    reg [6:0] opcode;
+    always @(*) begin
+        imm_gen_out = 0;
+        opcode = part_of_inst[6:0];
+        
+        case(opcode)
+            `ARITHMETIC_IMM: begin
+                imm_gen_out = {{21{part_of_inst[31]}}, part_of_inst[30:20]};
+            end
+            `LOAD: begin
+                imm_gen_out = {{21{part_of_inst[31]}}, part_of_inst[30:20]};
+            end
+            `JALR: begin
+                imm_gen_out = {{21{part_of_inst[31]}}, part_of_inst[30:21], 1'b0};
+            end
+            `STORE: begin
+                imm_gen_out = {{21{part_of_inst[31]}}, part_of_inst[30:25], part_of_inst[11:7]};
+            end
+            `BRANCH: begin
+                imm_gen_out = {{20{part_of_inst[31]}}, part_of_inst[7], part_of_inst[30:25], part_of_inst[11:8], 1'b0};
+            end
+            `JAL: begin
+                imm_gen_out = {{12{part_of_inst[31]}}, part_of_inst[19:12], part_of_inst[20], part_of_inst[30:21], 1'b0};
+            end
+            default: begin
+                imm_gen_out = 0;
+            end
+        endcase
+    end
+
+    endmodule
