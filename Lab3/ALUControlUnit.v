4bba1ccc095cc1451f2df9366ee7fadf5ff471dd
LYoooJ <limyoojin@postech.ac.kr>
2024-03-28 21:49:27 +0900

    modify ALUControlUnit(move location of assiging value into always)

--- Lab3/ALUControlUnit.v
+++ Lab3/ALUControlUnit.v
@@ -5,12 +5,16 @@ module ALUControlUnit (input [31:0] part_of_inst,
                        input [1:0] alu_op,
                        output reg[3:0] alu_control_lines); 
 
-    reg sign = part_of_inst[30];
-    reg [2:0] funct3 = part_of_inst[14:12]; 
-    reg [6:0] opcode = part_of_inst[6:0];
+    reg sign;
+    reg [2:0] funct3; 
+    reg [6:0] opcode;
 
     always@(*) begin
         alu_control_lines = 4'b1111;
+
+        sign = part_of_inst[30];
+        funct3 = part_of_inst[14:12]; 
+        opcode = part_of_inst[6:0];
 
         if(alu_op == 2'b00) begin
             alu_control_lines = `ADD;
@@ -21,7 +25,6 @@ module ALUControlUnit (input [31:0] part_of_inst,
         else begin 
             case(opcode)
                     `ARITHMETIC: begin
-                        $display("ARITHMETIC");
                         if(funct3 == `FUNCT3_ADD && sign == 0) alu_control_lines = `ADD;
                         else if(funct3 == `FUNCT3_SUB && sign == 1) alu_control_lines = `SUB;
                         else if(funct3 == `FUNCT3_SLL) alu_control_lines = `SLL;
@@ -32,7 +35,6 @@ module ALUControlUnit (input [31:0] part_of_inst,
                         else begin end
                     end
                     `ARITHMETIC_IMM: begin
-                        $display("ARITHMETIC_IMM");
                         if(funct3 == `FUNCT3_ADD) alu_control_lines = `ADD;
                         else if(funct3 == `FUNCT3_SLL) alu_control_lines = `SLL;
                         else if(funct3 == `FUNCT3_XOR) alu_control_lines = `XOR;
@@ -41,20 +43,7 @@ module ALUControlUnit (input [31:0] part_of_inst,
                         else if(funct3 == `FUNCT3_SRL) alu_control_lines = `SRL;
                         else begin end
                     end
-                    // `LOAD: begin
-                    //     alu_control_lines = `ADD;
-                    // end
-                    // `JALR: begin
-                    //     alu_control_lines = `ADD;
-                    // end
-                    // `JAL: begin
-                    //     alu_control_lines = `ADD;
-                    // end
-                    // `STORE: begin
-                    //     alu_control_lines = `ADD;
-                    // end
                     `BRANCH: begin
-                        $display("BRANCH");
                         if(funct3 == `FUNCT3_BEQ) alu_control_lines = `BEQ; //SUB
                         else if(funct3 == `FUNCT3_BNE) alu_control_lines = `BNE;
                         else if(funct3 == `FUNCT3_BLT) alu_control_lines = `BLT;
