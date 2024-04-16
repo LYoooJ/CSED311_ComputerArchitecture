48cebfd87c71c6ffa12bc7ffe267aaaf24f1ade3
LYoooJ <limyoojin@postech.ac.kr>
2024-04-16 22:29:18 +0900

    add modules

---
+++ Lab4_1/ControlUnit.v
@@ -0,0 +1,52 @@
+`include "opcodes.v"
+
+module control_unit(
+    input [6:0] part_of_inst,   // input
+    output reg mem_read,        // output
+    output reg mem_to_reg,      // output
+    output reg mem_write,       // output
+    output reg alu_src,         // output
+    output reg write_enable,    // output
+    output reg [1:0] alu_op,    // output
+    output reg is_ecall         // output
+    );     
+
+    always@(*) begin
+      mem_read = 0; 
+      mem_to_reg = 0;
+      mem_write = 0; 
+      alu_src = 0; 
+      write_enable = 0; 
+      alu_op = 2'b00;
+      is_ecall = 0;
+
+      case(part_of_inst)
+        `ARITHMETIC: begin 
+            write_enable = 1;
+            alu_op = 2'b10;
+        end  
+        `ARITHMETIC_IMM: begin
+            write_enable = 1;
+            alu_src = 1;
+            alu_op = 2'b10;
+        end
+        `LOAD: begin
+            write_enable = 1;
+            alu_src = 1;
+            mem_to_reg = 1;
+            mem_read = 1;
+            alu_op = 2'b00;
+        end
+        `STORE: begin
+            mem_write = 1;
+            alu_src = 1;
+            alu_op = 2'b00;
+        end
+        `ECALL: begin
+            is_ecall = 1;
+        end
+        default: begin
+        end
+      endcase
+    end
+endmodule
---
+++ Lab4_1/mux_4x1.v
@@ -0,0 +1,22 @@
+module mux_4x1 (input [31:0] input_1, 
+                input [31:0] input_2,
+                input [31:0] input_3,
+                input [31:0] input_4,
+                input [1:0] control, 
+                output reg [31:0] mux_out);
+
+always @(*) begin
+    if (control == 2'b00) begin 
+        mux_out = input_1;
+    end
+    else if(control == 2'b01) begin
+        mux_out = input_2;
+    end
+    else if(control == 2'b10) begin
+        mux_out = input_3;
+    end
+    else begin 
+        mux_out = input_4;
+    end
+end
+endmodule
